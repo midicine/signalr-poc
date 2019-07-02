021d2025216b547b3d2644d249e68e5e4a90ac4a
Ming Zhu <ming_zhu@executivecentre.com>
2019-07-02 15:16:01 +0800

    WIP

---
+++ SignalRPoc/SignalRPoc/Hubs/MessageHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Bogus;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRPoc.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public async Task GetMessage()
+        {
+            //await Task.Delay(5000);
+            await Clients.Caller.SendAsync("ReceiveMessage", $"[{DateTime.Now:s}] {new Faker().Address.FullAddress()} with connection {Context.ConnectionId}");
+        }
+    }
+}
---
+++ SignalRPoc/SignalRPoc/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/MessageHub").build();
+
+connection.on("ReceiveMessage", function (message) {
+    $("#messagesList").append($("<li class='list-group-item'>" + message + "</li>"));
+});
+
+connection
+    .start()
+    .then(function(){
+        connection
+            .invoke("GetMessage")
+            .catch(function (err) {
+                return console.error(err.toString());
+            });
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
